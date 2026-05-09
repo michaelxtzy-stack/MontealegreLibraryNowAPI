afc4b70d0910453dddcecd737ec319fc0efd4ed5
michaelxtzy-stack <michaelxtzy@gmail.com>
2026-05-09 18:36:04 +0800

    Update Program.cs

--- MontealegreLibraryNowAPI/Program.cs
+++ MontealegreLibraryNowAPI/Program.cs
@@ -13,11 +13,10 @@ var port = Environment.GetEnvironmentVariable("port") ?? "8080";
 app.Urls.Add($"http://+{port}");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.MapControllers();   
